16c6c7fb494a5f4246e7b72817280853bd03ab75
Stiven <olajhace2002@gmail.com>
2023-11-23 18:12:07 -0500

    submit

--- src/main/java/recursos/controladores/DestinosAdminController.java
+++ src/main/java/recursos/controladores/DestinosAdminController.java
@@ -1,4 +1,21 @@
 package recursos.controladores;
 
-public class DestinosAdminController {
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+import javax.swing.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DestinosAdminController implements Initializable {
+
+    @FXML
+    private ComboBox<String> comboBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        comboBox.setItems(FXCollections.observableArrayList("CALIDO", "TEMPLADO", "POLAR"));
+    }
 }
